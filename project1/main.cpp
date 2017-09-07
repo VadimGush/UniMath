d3b6c3bb884a8464fed56163e9df05d2d26d3528
Vadim Gush <gushvadim@gmail.com>
2017-09-07 19:06:08 +0400

    Last version of program

--- project1/main.cpp
+++ project1/main.cpp
@@ -1,63 +1,76 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 using namespace std;
 
-// Global variables
 const float v = 6;
-// Input data
+// Входные данные
 float epsilon = pow(10,-4);
 float a = 0;
 float b = 1;
+int segments = 5;
 
-// Prototypes of functions
+// Прототипы используемых функций
 float sum(float x, int* count);
-int fact(int n);
+double fact(int n);
 void input();
+void printLine();
 
 int main() {
-	// Read input data
-	input();
+	input(); // Читаем входные данные 
 
-	int i_val = 7;
-	cout << "Sum: " << sum(0.01f, &i_val) << endl;
-	cout << "Summands: " << i_val << endl;
+	// Рисуем линию сверху
+	printLine();
+	// Выводим значения x на экран
+	float delta = (b - a) / segments;
+	for (int i = 0; i <= segments; i++) {
+		cout << setw(20) << left << a + delta * i;
+	}
+	cout << endl;
+	// Выводим значения суммы на экран
+	int counts[segments+1];
+	for (int i = 0; i <= segments; i++) {
+		cout << setw(20) << left << sum(a + delta * i, &counts[i]);
+	}
+	cout << endl;
+	// Выводим массив количества слагаемых
+	for (int i = 0; i <= segments; i++) {
+		cout << setw(20) << left << counts[i];
+	}
+	cout << endl;
+	// Рисуем линию снизу
+	printLine();
+
 	return 0;
 }
 
 /*
- * Calculates the sum
- * x - variable
- * count - pointer to count of summands 
+ * Вычесляем сумму, передавая аргумент x и ссылку на переменную 
+ * хранящую количество слагаемых для данной функции
  */
 float sum(float x, int* count) {
-	float sum = 0;
-
+	double sum = 0;
 	int i = 1;
+	// Считаем сумму до тех пор, пока один из элементов не станет меньше epsilon
 	while (true) {
-		float element = 
-			pow(-1,i) * // sign of element
-			(pow(v*x,i+(i-1)) / (float)fact(i+(i-1)));
+		double element = pow(-1,i) * (pow(v*x,i+(i-1)) / fact(i+(i-1)));
 
-		cout << i << ": " << pow(v*x,i+(i-1)) << " , " << fact(i+(i-1)) << endl;
-
-		if (i > 20) break;
 		if (abs(element) < epsilon) break;
 		else {
-			sum += element;
+			sum += element; 
 			i++;
 		}
 	}
-	*count = i-1; // set number of summands
+	*count = i-1;
 
 	return sum;
 }
 
-/*
- * Calculates the factorial
- * n - input number
+/**
+ * Считаем факториал
  */
-int fact(int n) {
-	int result = 1;
+double fact(int n) {
+	double result = 1;
 	for (int counter = 1; counter <= n; counter++) {
 		result *= counter;
 	}
@@ -65,17 +78,32 @@ int fact(int n) {
 }
 
 /**
- * Reads input data
+ * Читаем входные данные
  */
 void input() {
 	float input = 0;
 	cout << "Write (0 for default value): ";
 	cin >> input;
 	if (input != 0) epsilon = input;
+
+	int seg = 0;
+	cout << "Count of segments (0 for default value): ";
+	cin >> seg;
+	if (seg != 0 & seg > 0) segments = seg;
+
 	cout << "Write a: ";
 	cin >> input;
 	a = input;
+
 	cout << "Write b: ";
 	cin >> input;
 	b = input;
+}
+
+void printLine() {
+	// Рисуем таблицу
+	for (int i = 0; i < 20 * (segments+1); i++) {
+		cout << "-";
+	}
+	cout << endl;
 }
