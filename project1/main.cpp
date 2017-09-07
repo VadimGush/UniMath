fc07e02430f3cf3e0ace4caaa17fed6eeb2d3fc5
Vadim Gush <gushvadim@gmail.com>
2017-09-07 16:05:22 +0400

    Hello world

---
+++ project1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// Global variables
+
+// Prototypes of functions
+
+int main() {
+	cout << "Hello world" << endl;
+	return 0;
+}
---
+++ project1/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Small script for compiling and running program
+g++ main.cpp -o program
+./program
